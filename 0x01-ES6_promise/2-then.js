be84dd41efa11cb89fb09bfdaf7a299f7c2ce6fa
Ammar Khaled Abdel Nour Ibrahim Ali <ammar.khaled.github@gmail.com>
2023-11-29 22:01:50 +0200

    Updated 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  const obj = { status: 200, body: 'Success' };
   return promise
-    .then(() => obj)
-    .catch(() => new Error())
-    .finally(() => { console.warn('Got a response from the API'); });
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.warn('Got a response from the API'));
 }
