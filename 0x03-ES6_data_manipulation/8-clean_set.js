c35fb996d9afb9d33c36e0ad985a8fbec5df40a8
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-12-05 05:36:21 +0000

    [Fix] compute length after checking validity

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,12 +2,14 @@
  * returns a string of all the set values that start with the given `startString`.
  */
 export default function cleanSet(set, startString) {
-  const len = startString.length;
-  if (!startString || !len) {
-    return '';
+  let result = '';
+
+  if (!startString || !startString.length) {
+    return result;
   }
 
   const filtered = [];
+  const len = startString.length;
   set.forEach((element) => {
     if (element && element.startsWith(startString)) {
       filtered.push(element.slice(len));
