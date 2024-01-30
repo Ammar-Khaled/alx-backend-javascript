0606f58cff82d8d187bad0f04fdd08051f30a312
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-01-30 19:35:01 +0000

    fix style

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,19 +1,17 @@
-const readDatabase = require("../utils");
-const path = process.argv[2];
+const readDatabase = require('../utils');
 
 /**
  * Controls student routes
  */
 class StudentsController {
   static getAllStudents(request, response) {
-    const path = process.argv.length > 2 ? process.argv[2] : "";
+    const path = process.argv.length > 2 ? process.argv[2] : '';
     readDatabase(path)
       .then((studentsByField) => {
         // sort fields
-        const sortedFields = Object.keys(studentsByField).sort((a, b) =>
-          a.toLowerCase().localeCompare(b.toLowerCase())
-        );
-        let payload = [];
+        const sortFunction = (a, b) => a.toLowerCase().localeCompare(b.toLowerCase());
+        const sortedFields = Object.keys(studentsByField).sort(sortFunction);
+        const payload = [];
         for (const field of sortedFields) {
           if (field) {
             console.log(field);
@@ -22,13 +20,13 @@ class StudentsController {
             payload.push(
               `Number of students in ${field}: ${
                 studentsByField[field].length
-              }. List: ${studentsByField[field].join(", ")}`
+              }. List: ${studentsByField[field].join(', ')}`,
             );
           }
         }
         response
           .status(200)
-          .send(`This is the list of our students\n${payload.join("\n")}`);
+          .send(`This is the list of our students\n${payload.join('\n')}`);
       })
       .catch((err) => {
         response.status(500).send(err.message);
@@ -36,16 +34,16 @@ class StudentsController {
   }
 
   static getAllStudentsByMajor(request, response) {
-    const path = process.argv.length > 2 ? process.argv[2] : "";
+    const path = process.argv.length > 2 ? process.argv[2] : '';
     const { major } = request.params;
-    if (major !== "CS" && major !== "SWE") {
-      response.status(500).send("Major parameter must be CS or SWE");
+    if (major !== 'CS' && major !== 'SWE') {
+      response.status(500).send('Major parameter must be CS or SWE');
       return;
     }
 
     readDatabase(path)
       .then((studentsByField) => {
-        response.status(200).send(`List: ${studentsByField[major].join(", ")}`);
+        response.status(200).send(`List: ${studentsByField[major].join(', ')}`);
       })
       .catch((err) => {
         response.status(500).send(err.message);
--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,12 +1,13 @@
-const express = require("express");
+const express = require('express');
+
 const router = express.Router();
-const AppController = require("../controllers/AppController");
-const StudentsController = require("../controllers/StudentsController");
+const AppController = require('../controllers/AppController');
+const StudentsController = require('../controllers/StudentsController');
 
 // Define routes
-router.get("/", AppController.getHomepage);
-router.get("/students", StudentsController.getAllStudents);
-router.get("/students/:major", StudentsController.getAllStudentsByMajor);
+router.get('/', AppController.getHomepage);
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
 export default router;
 module.exports = router;
