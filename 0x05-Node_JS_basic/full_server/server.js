e5ea03a025dbec4a5bffaecd45306889d5887548
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-01-30 19:28:44 +0000

    fix style

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,5 +1,6 @@
-const router = require("./routes/index");
-const express = require("express");
+const express = require('express');
+const router = require('./routes/index');
+
 const app = express();
 app.use(router);
 const port = 1245;
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require('fs');
 /**
  * Reads the data of students in a CSV data file.
  * @param {String} path The path to the CSV data file.
@@ -6,43 +6,42 @@ const fs = require("fs");
  *   String: {firstname: String}[]
  * }>}
  */
-const readDatabase = (path) =>
-  new Promise((resolve, reject) => {
-    if (!path) {
-      reject(new Error("Cannot load the database"));
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  if (!path) {
+    reject(new Error('Cannot load the database'));
+  }
+  fs.readFile(path, 'utf-8', (error, data) => {
+    if (error) {
+      reject(new Error('Cannot load the database'));
     }
-    fs.readFile(path, "utf-8", (error, data) => {
-      if (error) {
-        reject(new Error("Cannot load the database"));
+
+    if (data) {
+      const students = data.split('\n');
+      // remove headers
+      students.shift();
+      // remove empty lines at the end of the file
+      let i = students.length - 1;
+      while (students[i] === '') {
+        students.pop();
+        i -= 1;
       }
 
-      if (data) {
-        const students = data.split("\n");
-        // remove headers
-        students.shift();
-        // remove empty lines at the end of the file
-        let i = students.length - 1;
-        while (students[i] === "") {
-          students.pop();
-          i -= 1;
-        }
+      const studentsByField = {};
 
-        const studentsByField = {};
-
-        for (const s of students) {
-          const studentData = s.split(",");
-          const firstName = studentData[0];
-          const field = studentData[3];
-          if (field in studentsByField) {
-            studentsByField[field].push(firstName);
-          } else {
-            studentsByField[field] = [firstName];
-          }
+      for (const s of students) {
+        const studentData = s.split(',');
+        const firstName = studentData[0];
+        const field = studentData[3];
+        if (field in studentsByField) {
+          studentsByField[field].push(firstName);
+        } else {
+          studentsByField[field] = [firstName];
         }
-        resolve(studentsByField);
       }
-    });
+      resolve(studentsByField);
+    }
   });
+});
 
 export default readDatabase;
 module.exports = readDatabase;
