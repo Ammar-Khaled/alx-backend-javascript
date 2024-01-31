c1a202a3e1c19a040e562d1949b004d2824724bc
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-01-31 17:54:29 +0000

    Use deepStrictEqual instead of equal

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,18 +4,18 @@ const calculateNumber = require("./0-calcul");
 
 describe("calculateNumber", () => {
   it("", () => {
-    assert.equal(calculateNumber(1, 2), 3);
+    assert.deepStrictEqual(calculateNumber(1, 2), 3);
   });
 
   it("", () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.deepStrictEqual(calculateNumber(1, 3.7), 5);
   });
 
   it("", () => {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.deepStrictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
   it("", () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.deepStrictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
