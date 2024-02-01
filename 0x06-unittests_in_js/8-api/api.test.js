a1797056400b0a6a5ee096eaabe07c49923fe4e4
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-02-01 18:04:47 +0000

    Update: Add more test cases

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,10 +2,27 @@ const request = require("request");
 const expect = require("chai").expect;
 
 describe("Index page", () => {
-  it("", () => {
-    request.get("http://localhost:7865", (_, res, body) => {
+  it("should have the correct status code", () => {
+    request.get("http://localhost:7865", (_, res) => {
       expect(res.statusCode).to.equal(200);
-      expect(body).to.equal("Welcome to the payment system");
+    });
+  });
+
+  it("should have the correct body", () => {
+    request.get("http://localhost:7865", (_, res, body) => {
+      expect(body).to.contain("Welcome to the payment system");
+    });
+  });
+
+  it("should have the correct content type", () => {
+    request("http://localhost:7865", (_, res) => {
+      expect(res.headers["content-type"]).to.equal("text/html; charset=utf-8");
+    });
+  });
+
+  it("should have the correct content length", () => {
+    request("http://localhost:7865", (_, res) => {
+      expect(res.headers["content-length"]).to.equal("29");
     });
   });
 });
