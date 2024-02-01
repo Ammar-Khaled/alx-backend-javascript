5bcd1619f69d68e4826b750476d9dcf8a5d14769
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-02-01 18:13:42 +0000

    Update: Add more test cases

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,23 +2,48 @@ const request = require("request");
 const expect = require("chai").expect;
 
 describe("Index page", () => {
-  it("", () => {
-    request.get("http://localhost:7865", (_, res, body) => {
+  it("should have the correct status code", () => {
+    request.get("http://localhost:7865", (_, res) => {
       expect(res.statusCode).to.equal(200);
-      expect(body).to.equal("Welcome to the payment system");
     });
   });
 
-  it("", () => {
+  it("should have the correct body", () => {
+    request.get("http://localhost:7865", (_, res, body) => {
+      expect(body).to.contain("Welcome to the payment system");
+    });
+  });
+
+  it("should have the correct content type", () => {
+    request("http://localhost:7865", (_, res) => {
+      expect(res.headers["content-type"]).to.equal("text/html; charset=utf-8");
+    });
+  });
+
+  it("should have the correct content length", () => {
+    request("http://localhost:7865", (_, res) => {
+      expect(res.headers["content-length"]).to.equal("29");
+    });
+  });
+});
+
+describe("Cart page", () => {
+  it("should have the correct status code and payload", () => {
     request.get("http://localhost:7865/cart/1", (_, res, body) => {
       expect(res.statusCode).to.equal(200);
       expect(body).to.equal("Payment methods for cart 1");
     });
   });
 
-  it("", () => {
-    request.get("http://localhost:7865/cart/a", (_, res, body) => {
+  it("should have error not found with status code 404", () => {
+    request.get("http://localhost:7865/cart/abc", (_, res, body) => {
       expect(res.statusCode).to.equal(404);
+    });
+  });
+
+  it("should have the correct body content with non number id parameter", () => {
+    request("http://localhost:7865/cart/abc", (_error, _response, body) => {
+      expect(body).to.contain("Cannot GET /cart/abc");
     });
   });
 });
