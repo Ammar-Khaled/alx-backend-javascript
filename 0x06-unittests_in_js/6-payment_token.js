595beec9d01732d16297286884585f28fac69177
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-02-01 19:19:21 +0000

    Update

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -4,9 +4,15 @@
  * @returns a resolved Promise representing success if success
  */
 const getPaymentTokenFromAPI = (success) => {
-  if (success) {
-    return Promise.resolve({ data: "Successful response from the API" });
-  }
+  const p = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ data: "Successful response from the API" });
+    } else {
+      reject("")
+    }
+  });
+
+  return p;
 };
 
 module.exports = getPaymentTokenFromAPI;
